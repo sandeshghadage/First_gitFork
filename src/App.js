0782efcf07a9400a0f0b15438a2bc10f40612a0c
sandeshghadage <sandeshghadage3@gmail.com>
2023-05-05 23:02:57 +0530

    pixel to rem functionality part done css pending

--- src/App.js
+++ src/App.js
@@ -1,8 +1,38 @@
 import "./App.css";
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { HiOutlineSwitchHorizontal } from "react-icons/hi";
 
 function App() {
-  return <div></div>;
+  const [rem, setRem] = useState(0);
+  const [pixel, setPixel] = useState(0);
+
+  useEffect(() => {
+    setPixel(rem * 16);
+  }, [rem]);
+
+  useEffect(() => {
+    setRem(pixel / 16);
+  }, [pixel]);
+
+  return (
+    <div className="mainBox">
+      <h2>PX to REM converter</h2>
+      <div className="inputPanel">
+        <div className="inputWithLabel">
+          <div>Pixel</div>
+          <input
+            onChange={(e) => setPixel(e.target.value)}
+            value={pixel}
+          ></input>
+        </div>
+        <HiOutlineSwitchHorizontal style={{ alignSelf: "center" }} />
+        <div className="inputWithLabel">
+          <div>REM</div>
+          <input onChange={(e) => setRem(e.target.value)} value={rem}></input>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
